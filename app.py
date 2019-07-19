d500b1c507b88a431b597df96184841230b06f20
Bhanu Dagar <iamdbhanu@gmail.com>
2019-07-19 16:34:27 +0530

    Update app.py

--- app.py
+++ app.py
@@ -9,6 +9,8 @@ def translate(word):
 		return data[word]
 	elif word.title() in data:
 		return data[word.title()]
+	elif word.upper() in data:
+		return data[word.upper()]
 	elif len(get_close_matches(word, data.keys())) > 0:
 		yn = input("Did you mean %s instead ? Enter Y if yes, or N if no: " % get_close_matches(word, data.keys())[0])
 		if yn is "Y":
